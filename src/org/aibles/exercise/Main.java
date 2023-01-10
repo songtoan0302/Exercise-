d514a0b83b7a1bfc0a74f4b863fcc1510beb46cc
Toanns <nguyensongtoan0302@gmail.com>
2023-01-11 01:35:17 +0700

    split method, method analysis to use again

--- src/org/aibles/exercise/Main.java
+++ src/org/aibles/exercise/Main.java
@@ -1,12 +1,28 @@
 package org.aibles.exercise;
 
+import java.util.Scanner;
 import org.aibles.exercise.service.ProcessNumber;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
-        ProcessNumber.showList(ProcessNumber.analysisNumberSequence());
+
+  public static void main(String[] args) {
+    while (true) {
+      Scanner scanner = new Scanner(System.in);
+      var checkRun = scanner.next();
+      if (checkRun.contains("quit")) break;
+      System.out.println("Enter divisibilityNumber:");
+      var divisibilityNumber = Integer.parseInt(scanner.next());
+      System.out.println("Enter surplusRequestDivisibility:");
+      var surplusRequestDivisibility = Integer.parseInt(scanner.next());
+      System.out.println("Enter unDivisibilityNumber:");
+      var unDivisibilityNumber = Integer.parseInt(scanner.next());
+      System.out.println("Enter surplusRequestUnDivisibility:");
+      var surplusRequestUnDivisibility = Integer.parseInt(scanner.next());
+      var stringNumber = ProcessNumber.buildStringNumber(
+          ProcessNumber.analysisNumberSequence(divisibilityNumber, surplusRequestDivisibility,
+              unDivisibilityNumber, surplusRequestUnDivisibility));
+      ProcessNumber.showString(stringNumber);
     }
+  }
+
 }
--- src/org/aibles/exercise/service/ProcessNumber.java
+++ src/org/aibles/exercise/service/ProcessNumber.java
@@ -4,51 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProcessNumber {
-    private static final int LIMIT_MIN_NUMBER_SEQUENCE = 10;
-    private static final int LIMIT_MAX_NUMBER_SEQUENCE = 200;
-    private static final int RESULT_DIVISIBILITY = 0;
+
+  private static final int LIMIT_MIN_NUMBER_SEQUENCE = 10;
+  private static final int LIMIT_MAX_NUMBER_SEQUENCE = 200;
+
+
+  /**
+   * method take a list after print each element of list
+   *
+   * @param showString
+   */
+  public static <T> void showString(String showString) {
+    System.out.println(showString);
+  }
 
     /**
-     * by topic. number first is number which all number result must satisfy divisibility for it
-     */
-    private static final int NUMBER_DIVISIBILITY = 7;
-
-    /**
-     * by topic. number first is number which all number result must satisfy not divisibility for it
-     */
-    private static final int NUMBER_NOT_DIVISIBILITY = 5;
-
-
-    /**
-     * method take a list after print each element of list
-     *
+     * build a string number follow request from list number
      * @param list
+     * @return a string number
+     * @param <T>
      */
-    public static void showList(List<Integer> list) {
-        if (list != null && !list.isEmpty()) {
-//            example 1
-            for (Integer integer : list) {
-                System.out.print(integer + ", ");
-            }
-//            example 2
-            for (int i = 0; i < list.size(); i++) {
-                System.out.print(list.get(i) + ", ");
-            }
-        }
+  public static <T> String buildStringNumber(List<T> list) {
+    StringBuilder result = new StringBuilder();
+    for (T element : list) {
+      if (element == list.get(list.size() - 1)) {
+        result.append(element);
+      } else {
+        result.append(element).append(", ");
+      }
     }
+    return result.toString();
+  }
 
-    /**
-     * method org.aibles.exercise.analysis a list array from 10 to 200 to find number divisibility for 7 and not divisibility for 5
-     *
-     * @return a list number after org.aibles.exercise.analysis
-     */
-    public static List<Integer> analysisNumberSequence() {
-        List<Integer> results = new ArrayList<>();
-        for (int i = LIMIT_MIN_NUMBER_SEQUENCE; i <= LIMIT_MAX_NUMBER_SEQUENCE; i++) {
-            if ((i % NUMBER_DIVISIBILITY == RESULT_DIVISIBILITY) && (i % NUMBER_NOT_DIVISIBILITY != RESULT_DIVISIBILITY)) {
-                results.add(i);
-            }
-        }
-        return results;
+  /**
+   * method org.aibles.exercise.analysis a list array from 10 to 200 to find number divisibility for
+   * 7 and not divisibility for 5
+   *
+   * @return a list number after org.aibles.exercise.analysis
+   */
+  public static List<String> analysisNumberSequence(int divisibilityNumber,
+      int surplusRequestDivisibility, int unDivisibilityNumber, int surplusRequestUnDivisibility) {
+    List<String> results = new ArrayList<>();
+    for (int i = LIMIT_MIN_NUMBER_SEQUENCE; i <= LIMIT_MAX_NUMBER_SEQUENCE; i++) {
+      if ((i % divisibilityNumber == surplusRequestDivisibility) && (i % unDivisibilityNumber
+          != surplusRequestUnDivisibility)) {
+        results.add(String.valueOf(i));
+      }
     }
+    return results;
+  }
+
 }
